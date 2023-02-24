ce23519ac344dc52ed19193bbca85018aa420079
AleksGor <avg_samara@mail.ru>
2023-02-24 21:02:19 +0400

    Finished Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число a");
+int numbera = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int numberb = Convert.ToInt32(Console.ReadLine());
+if (numbera == numberb * numberb)
+{
+   Console.WriteLine("Число а является квадратом числа b"); 
+}
+else
+{
+   Console.WriteLine("Число a не является квадратом числа b");  
+}
