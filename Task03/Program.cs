7eb7e0259d5ea6dca9d57e069030d154ed45c89f
AleksGor <avg_samara@mail.ru>
2023-02-24 21:18:27 +0400

    Finished Task03

---
+++ Task03/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 1)
+{
+    Console.WriteLine("Это понедельник"); 
+}
+else if (number == 2)
+{
+    Console.WriteLine("Это вторник"); 
+}
+else if (number == 3)
+{
+    Console.WriteLine("Это среда"); 
+}
+else if (number == 4)
+{
+    Console.WriteLine("Это четверг"); 
+}
+else if (number == 5)
+{
+    Console.WriteLine("Это пятница");
+}
+else if (number == 6)
+{
+    Console.WriteLine("Это суббота"); 
+}
+else if (number == 7)
+{
+    Console.WriteLine("Это воскресенье"); 
+}
+else
+{
+    Console.WriteLine("Такого дня недели не существует"); 
+}
+
