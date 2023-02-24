68e8c4b7e23772f1a90ede5d91eacb8cc64b5c75
AleksGor <avg_samara@mail.ru>
2023-02-24 22:42:50 +0400

    Finished Task07

---
+++ Task07/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number<0) number *= -1;
+if (number>99 && number < 1000)
+{
+    int result = number % 10;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Нужно ввести трехзначное число");
+}
