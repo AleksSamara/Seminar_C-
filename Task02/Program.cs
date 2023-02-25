b6a5113f06bdad0b27c7430b918e77a6467924a9
AleksGor <avg_samara@mail.ru>
2023-02-25 16:32:11 +0400

    Finished Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите первое число a");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число b");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine("Число a больше числа b"); 
+}
+else if (numberB > numberA)
+{
+    Console.WriteLine("Число b больше числа a"); 
+}
+else
+{
+    Console.WriteLine("Числа a и b одинаковые"); 
+}
