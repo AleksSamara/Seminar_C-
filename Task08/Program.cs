6ee455ac927878178561a713249e19155c3b42d0
AleksGor <avg_samara@mail.ru>
2023-02-25 17:40:01 +0400

    Finished Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите натуральное число N больше 1");
+int N = Convert.ToInt32(Console.ReadLine());
+int index = 2;
+if (N > 1)
+{
+    Console.WriteLine("В диапозоне от 1 до N найдены чётные числа: ");
+    while (index <= N)
+    {
+       Console.Write(index);
+       Console.Write(", ");
+       index = index + 2;
+    }
+    Console.WriteLine();
+}
+else
+{
+    Console.WriteLine("Введите натуральное число больше 1");
+}
