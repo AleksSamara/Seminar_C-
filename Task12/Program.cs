6547133ce5e0af1ba0a8f0b01a7da45d3614a1be
AleksGor <avg_samara@mail.ru>
2023-02-27 22:56:03 +0400

    Added Task12

---
+++ Task12/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число a");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int result = numberA % numberB;
+if (result == 0)
+{
+   Console.WriteLine($"Число {numberA} кратно числу {numberB}"); 
+}
+else
+{
+   Console.WriteLine($"Число {numberA} некратно числу {numberB}, остаток от деления {result}");  
+}
+
+
+int Multiplicity(int numA, int numB)
+{
+    return numA % numB;
+}
+
+
