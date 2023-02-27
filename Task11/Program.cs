9d48c0dedced9d8266a7fd9f15b1a16a3cdadd2a
AleksGor <avg_samara@mail.ru>
2023-02-27 22:04:02 +0400

    Added Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,7 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из отрезка 100-999 -> {number}");
+int firstDigit = number / 100;
+int thirdDigit = number % 10;
+
+int result = firstDigit * 10 + thirdDigit;
+Console.WriteLine($"Результат -> {result}");
