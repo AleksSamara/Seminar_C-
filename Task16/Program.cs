5a1d8109f21a707b772aeff0d616a85444d0cd45
AleksGor <avg_samara@mail.ru>
2023-03-01 22:30:39 +0400

    Finished Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите натуральное число a: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число b: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+bool isSquare = IsSquare(number1, number2);
+Console.WriteLine(isSquare ? "Да" : "Нет");
+
+
+
+bool IsSquare(int num1, int num2)
+{
+    return num1 == num2 * num2 || num2 == num1 * num1;
+}
