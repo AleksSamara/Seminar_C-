0be2c77f15b47060aae39bb5f69f3daaef3b91d1
AleksGor <avg_samara@mail.ru>
2023-03-02 21:46:34 +0400

    Finished Task18

---
+++ Task18/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите номер четверти: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Range(quarter));
+
+
+string Range(int num)
+{
+    if(num == 1) return "Диапозон значений: X > 0 и Y > 0";
+    if(num == 2) return "Диапозон значений: X < 0 и Y > 0";
+    if(num == 3) return "Диапозон значений: X < 0 и Y < 0";
+    if(num == 4) return "Диапозон значений: X > 0 и Y < 0";
+    return "Такой четверти не существует";
+}
+
