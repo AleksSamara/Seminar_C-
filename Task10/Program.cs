d40dc0239d3e6b6c4b616b7e3438affe14ba7a3d
AleksGor <avg_samara@mail.ru>
2023-03-02 19:49:00 +0400

    Finished Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100 || number > 999)
+{
+    Console.WriteLine("Надо ввести трехзначное число");
+}
+else
+{
+    int secondDigit = SecondDigit(number);
+    Console.WriteLine($"Вторая цифра введённого числа -> {secondDigit}");
+}
+
+
+int SecondDigit(int num)
+{
+    int firstTwoDigit = num / 10;
+    int second = firstTwoDigit % 10;
+    return second;
+}
