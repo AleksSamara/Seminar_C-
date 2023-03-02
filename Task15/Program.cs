1c48d53259d7f529b81763debcbd3bff2c28ba9a
AleksGor <avg_samara@mail.ru>
2023-03-02 20:20:20 +0400

    Finished Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите порядковый номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1 || number > 7)
+{
+    Console.WriteLine("Такого порядкового дня недели не существует");
+}
+else
+{
+    bool freeDay = FreeDay(number);
+    Console.WriteLine(freeDay ? "Да" : "Нет");
+}
+
+
+bool FreeDay(int num)
+{
+    return num == 6 || num == 7;
+}
+
