79437a6583c352feb850e972b5232ea4334acec8
AleksGor <avg_samara@mail.ru>
2023-03-02 20:09:19 +0400

    Finished Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 99)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    int thirdDigit = ThirdDigit(number);
+    Console.WriteLine($"Третья цифра введённого числа -> {thirdDigit}");
+}
+
+
+int ThirdDigit(int num)
+{
+    int third = num % 10;
+    return third;
+}
