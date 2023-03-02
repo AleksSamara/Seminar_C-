8f4552d24ea28a602e2b3d36237f8f2516331a87
AleksGor <avg_samara@mail.ru>
2023-03-02 22:24:14 +0400

    Finished Task20

---
+++ Task20/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите координаты первой точки");
+Console.Write("X: ");
+int xCoordinate1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int yCoordinate1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки");
+Console.Write("X: ");
+int xCoordinate2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int yCoordinate2 = Convert.ToInt32(Console.ReadLine());
+
+
+double length = Length(xCoordinate1, yCoordinate1, xCoordinate2, yCoordinate2);
+Console.WriteLine(Math.Round(length, 2, MidpointRounding.ToZero));
+
+
+
+double Length(int x1, int y1, int x2, int y2)
+{
+    return Math.Sqrt((x2-x1) * (x2-x1) + (y2-y1) * (y2-y1));
+}
+
