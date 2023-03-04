aeffde39527cf5d9fe51a0ace1dcdcc766fc3fe5
AleksGor <avg_samara@mail.ru>
2023-03-04 12:22:17 +0400

    Finished Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите натуральное пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 9999 && number < 100000)
+{
+    bool overturn = Overturn(number);
+    Console.WriteLine(overturn ? "Да" : "Нет");
+}
+else
+{
+    Console.WriteLine("Необходимо ввести натуральное пятизначное число");
+}
+
+bool Overturn(int num)
+{
+    return num / 10000 == num % 10 && num / 1000 % 10 == num / 10 % 10;
+}
