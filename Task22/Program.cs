262adf74f4dfb6d6ae8e6c7ced8b2f903bba460a
AleksGor <avg_samara@mail.ru>
2023-03-04 10:55:05 +0400

    Finished Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+TableSquare(number);
+
+void TableSquare(int num)
+{
+    int index = 1;
+    while (index <= num)
+    {
+        Console.WriteLine($"{index, 4}   {index * index, 4}");
+        index = index + 1;
+    }
+}
