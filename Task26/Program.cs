16a571945b09f24c80522745f3a76edf53a4be71
AleksGor <avg_samara@mail.ru>
2023-03-06 22:11:16 +0400

    Finished Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int quantDigit = QuantDigit(number);
+    Console.WriteLine($"В числе {number} количество разрядов = {quantDigit}");
+
+int QuantDigit(int num)
+{
+    int sum = 0;
+    while (num != 0)
+    {
+        num = num / 10;
+        sum = sum + 1;
+    }
+    return sum;
+}
+
