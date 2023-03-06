40539ed9eb7f053c435c12760d7927d421345f2c
AleksGor <avg_samara@mail.ru>
2023-03-06 21:36:04 +0400

    Finished Task24

---
+++ Task24/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 1)
+{
+    int sumNumbers = SumNumbers(number);
+    Console.WriteLine($"Сумма чисел от единицы до {number} = {sumNumbers}");
+}
+else
+{
+    Console.WriteLine("Необходимо ввести целое положительное число");
+}
+
+int SumNumbers(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
