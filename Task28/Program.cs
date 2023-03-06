f705d7aa79d9ff185ed85a52c45fd507c712f243
AleksGor <avg_samara@mail.ru>
2023-03-06 22:22:49 +0400

    Finished Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 1)
+{
+    int fact = Fact(number);
+    Console.WriteLine($"Факториал от единицы до {number} = {fact}");
+}
+else
+{
+    Console.WriteLine("Необходимо ввести целое положительное число");
+}
+
+int Fact(int num)
+{
+    int result = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+
