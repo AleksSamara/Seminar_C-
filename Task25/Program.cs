2c935472d9d6d1cd0f58c236158d4d9637919c20
AleksGor <avg_samara@mail.ru>
2023-03-07 22:36:54 +0400

    Finished Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите целое число a: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число b: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number2 > 0)
+{
+    int degree = Degree(number1, number2);
+    Console.WriteLine($"Число {number1} в степени {number2} = {degree}");
+}
+else
+{
+    Console.WriteLine("Необходимо ввести натуральное число b");
+}
+
+
+int Degree(int num1, int num2)
+{
+    int result = 1;
+    for (int i = 1; i <= num2; i++)
+    {
+        result = result * num1;
+    }
+        return result;
+}
