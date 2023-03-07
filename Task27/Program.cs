bb2d486c5330e02d3c68beccddebafb7cc1f31a5
AleksGor <avg_samara@mail.ru>
2023-03-07 22:58:43 +0400

    Finished Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 1)
+{
+    int sumdigit = SumDigit(number);
+    Console.WriteLine($"Сумма цифр числа {number} = {sumdigit}");
+}
+else
+{
+    Console.WriteLine("Необходимо ввести натуральное число");
+}
+
+
+int SumDigit(int num)
+{
+    int sum = 0;
+    while (num >= 1)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
