ef5d537165fda913c3d76b301354bf291cba6865
AleksGor <avg_samara@mail.ru>
2023-03-12 10:43:24 +0400

    Finished Task32

---
+++ Task32/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Введите размер массива - любое натуральное число: ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateArray(sizeArray, -100, 100);
+PrintArray(array);
+
+int[] changeNumber = ChangeNumber(array);
+PrintArray(changeNumber);
+
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+int[] ChangeNumber(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = arr[i] * -1;
+    }
+    return arr;
+}
+
