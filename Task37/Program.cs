8dfd79b6435844ce77c409cad0d76ef1a3a22d4f
AleksGor <avg_samara@mail.ru>
2023-03-12 12:33:54 +0400

    Finished Task37

---
+++ Task37/Program.cs
@@ -0,0 +1,42 @@
+﻿int[] array = CreateArray(9, 0, 10);
+PrintArray(array);
+int[] newArray = NewArrayMultiple(array);
+PrintArray(newArray);
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+int[] NewArrayMultiple(int[] arr)
+{
+    int size = arr.Length / 2;
+    if (arr.Length % 2 != 0) size += 1;
+    int[] newArr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        newArr[i] = arr[i] * arr[arr.Length-i-1];
+    }
+    if (arr.Length % 2 != 0) newArr [size-1] = arr[arr.Length/2];
+    return newArr;
+}
+
