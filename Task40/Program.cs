eb868436bc72bca60104f427c000e12e1ebbe111
AleksGor <avg_samara@mail.ru>
2023-03-13 22:38:31 +0400

    Finished Task40

---
+++ Task40/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое натуральное число a: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе натуральное число b: ");
+int numberB= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье натуральное число c: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+bool triangle = Triangle(numberA, numberB, numberC);
+Console.WriteLine(triangle ? "Эти числа могут быть сторонами треугольника" : "Эти числа не могут быть сторонами треугольника");
+
+bool Triangle(int num1, int num2, int num3)
+{
+    if (num1 < num2 + num3 && num2 < num1 + num3 && num3 < num1 + num2) return true;
+    else return false;
+}
