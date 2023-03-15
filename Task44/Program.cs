6233175e2c78a25a9aed914a3615560c34ee5fad
AleksGor <avg_samara@mail.ru>
2023-03-15 20:19:05 +0400

    Finished Task44

---
+++ Task44/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите натуральное число N больше единицы для вывода первых N-чисел ряда Фибоначчи: ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+
+if (sizeArray > 1)
+{
+    int[] array = CreateArray(sizeArray);
+    PrintArray(array);
+}
+else Console.WriteLine("Надо ввести натуральное число больше единицы");
+
+
+
+int[] CreateArray(int size)
+{
+    int[] arr = new int[size];
+    arr[0] = 0;
+    arr[1] = 1;
+    for (int i = 2; i < size; i++)
+    {
+        arr[i] = arr [i - 1] + arr[i - 2];
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
