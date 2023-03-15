6d98d5de407b451dedb580ee9b6a0e3f69ad0e04
AleksGor <avg_samara@mail.ru>
2023-03-15 19:41:33 +0400

    Finished Task42

---
+++ Task42/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(NumberConversion(number));
+
+// string NumberConversion(int num)
+// {
+//     string result = string.Empty;
+//     while (num > 0)
+//     {
+//         result = num % 2 + result;
+//         num = num / 2;
+//     }
+//     return result;
+// }
+
+int NumberConversion(int num)
+{
+    int result = 0, dec = 1;
+    while (num > 0)
+    {
+        result = result + num % 2 * dec;
+        dec *= 10;
+        num = num / 2;
+    }
+    return result;
+}
+
