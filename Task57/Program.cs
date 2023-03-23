2399f31476ef77d4a8b17ab896c3cff64866401f
AleksGor <avg_samara@mail.ru>
2023-03-23 22:14:47 +0400

    Finished Task57

---
+++ Task57/Program.cs
@@ -0,0 +1,86 @@
+﻿Console.WriteLine("Введите натуральное число - количество строк массива: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число - количество столбцов массива: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int[,] array2d = CreateMatrixRndInt(numberA, numberB, 0, 10);
+PrintMatrix(array2d);
+Console.WriteLine();
+int[] newarray = TransformMatrixToArray(array2d);
+Array.Sort(newarray);
+PrintArray(newarray);
+Dictionary(newarray);
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix [i, j] = rand.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 5} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[] TransformMatrixToArray(int[,] matrix)
+{
+    int [] array = new int [matrix.Length];
+    int count = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            array[count] = matrix [i, j];
+            count += 1;
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+void Dictionary(int[] arr)
+{
+    int count = 1;
+    int value = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if(arr[i] == value) count++;
+        else
+        {
+            Console.WriteLine($"{value} ---> {count}");
+            value = arr[i];
+            count = 1;
+        }
+    }
+    Console.WriteLine($"{value} ---> {count}");
+}
+
