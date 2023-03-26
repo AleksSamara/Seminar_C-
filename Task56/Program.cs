7847a79cc9a12240717e09c916c161a0b906d311
AleksGor <avg_samara@mail.ru>
2023-03-26 11:17:30 +0400

    Finished Task56

---
+++ Task56/Program.cs
@@ -0,0 +1,71 @@
+﻿Console.WriteLine("Введите натуральное число - количество строк массива: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число - количество столбцов массива: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+
+int[,] array2d = CreateMatrixRndInt(numberA, numberB, 1, 10);
+PrintMatrix(array2d);
+Console.WriteLine();
+int[] array = SumElementsArray(array2d);
+Console.WriteLine($"Строка с наименьшей суммой элементов - {RowMinSum(array)}");
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix [i, j] = rand.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 5} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[] SumElementsArray(int[,] matrix)
+{
+    int[] arr = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum = sum + matrix[i, j];
+        }
+        arr[i] = sum;
+    }
+    return arr;
+}
+
+int RowMinSum(int[] arr)
+{
+    int min = arr[0];
+    int result = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] < min)
+        {
+            min = arr[i];
+            result = i;
+        }
+    }
+    return result;
+}
+
