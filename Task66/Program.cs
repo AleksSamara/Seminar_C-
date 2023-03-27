674c348b2065f7b53d6049411d6e7a48f5763c86
AleksGor <avg_samara@mail.ru>
2023-03-27 21:50:53 +0400

    Finished Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите целое положительное число M");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое положительное число N");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if (numberM > numberN)
+{
+    int sumNumber = SumNumber(numberM, numberN);
+    Console.WriteLine(sumNumber);
+}
+else if (numberM < numberN)
+{
+    int sumNumber = SumNumber(numberN, numberM);
+    Console.WriteLine(sumNumber);
+}
+else Console.WriteLine("Введены одинаковые числа");
+
+
+int SumNumber(int num1, int num2)
+{
+    if(num1 < num2) return 0;
+    else return num1 + SumNumber(num1 - 1, num2);
+}
+
+
