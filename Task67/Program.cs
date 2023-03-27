54872a4ea900491212e27de84a3fe410e43c591a
AleksGor <avg_samara@mail.ru>
2023-03-27 20:32:49 +0400

    Finished Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = SumDidgitalsNumber(number);
+Console.WriteLine(result);
+
+int SumDidgitalsNumber(int num)
+{
+    if(num == 0) return 0;
+    else return num % 10 + SumDidgitalsNumber(num / 10);
+}
+
