d6e3a54bcbd83f3fa731cab8739a5e9d3737ccc0
AleksGor <avg_samara@mail.ru>
2023-03-27 21:27:08 +0400

    Finished Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    NaturalNumbers(number);
+    Console.WriteLine();
+}
+else Console.WriteLine("Надо ввести целое положительное число");
+
+
+void NaturalNumbers(int num)
+{
+    if(num == 0) return;
+    Console.Write($"{num} ");
+    NaturalNumbers(num - 1);   
+}
