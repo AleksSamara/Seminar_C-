ae992119efaf3f5f6dc0b5759a0460e8a10f1dbe
AleksGor <avg_samara@mail.ru>
2023-03-27 20:44:13 +0400

    Finished Task69

---
+++ Task69/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите целое положительное число A");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое положительное число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int multi = MultiNumbers(numberA, numberB);
+Console.WriteLine(multi);
+
+int MultiNumbers(int num1, int num2)
+{
+    if(num2 == 0) return 1;
+    else return checked (num1 * MultiNumbers(num1, num2 - 1));
+}
+
